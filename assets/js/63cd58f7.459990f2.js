dd18c3d70419d7c7f63f028afdf34aebf1e1dbf7
benjaroa <benjaroa@gmail.com>
2023-03-29 05:40:29 +0000

    Deploying to gh-pages from @ bankingbox-ai/wiki@34224a49ee089c2aee5950bcd39e69f00bc043e0 🚀

---
+++ assets/js/63cd58f7.459990f2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[590],{8373:e=>{e.exports=JSON.parse('{"label":"ux/ui","permalink":"/blog/tags/ux-ui","allTagsPath":"/blog/tags","count":1}')}}]);
