7e6f35193bf0b5c4666369d625ba2d71ad5ad2cb
Benjamin Roa <broa@bankingbox.com>
2023-03-29 02:50:03 -0300

    fix: missing base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const getConfig = async () => {
     url: 'https://your-docusaurus-test-site.com',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    baseUrl: '/wiki',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
